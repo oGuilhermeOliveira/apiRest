c3dbc18d03a92fccefd5ecdf3e9db2c66c07f8fb
Ryan-Bergstron <ryanbergs2005@gmail.com>
2026-05-08 09:27:19 -0300

    corrigindo o security

--- src/config/security.js
+++ src/config/security.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
   windowMs: 60 * 1000,
   max: 5,
-  message: "Muitas requisições cabra safado"
+  message: "Muitas requisições cabra safado - Guilherme Oliveira - RGM: 2417427"
 });
 
 app.use(limiter);
